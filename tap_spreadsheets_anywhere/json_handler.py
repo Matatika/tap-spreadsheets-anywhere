1161d33fda8bf214aebfa56742f100a01b695869
danielpdwalker <danielpdwalker@gmail.com>
2023-09-07 11:30:42 +0100

    Fixed json_handler logic

--- tap_spreadsheets_anywhere/json_handler.py
+++ tap_spreadsheets_anywhere/json_handler.py
@@ -6,19 +6,18 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 def generator_wrapper(root_iterator):
-    for obj in root_iterator:
-        to_return = {}
-        for key, value in obj.items():
-            if key is None:
-                key = '_smart_extra'
+    to_return = {}
+    for key, value in root_iterator.items():
+        if key is None:
+            key = '_smart_extra'
 
-            formatted_key = key
-            # remove non-word, non-whitespace characters
-            formatted_key = re.sub(r"[^\w\s]", '', formatted_key)
-            # replace whitespace with underscores
-            formatted_key = re.sub(r"\s+", '_', formatted_key)
-            to_return[formatted_key.lower()] = value
-        yield to_return
+        formatted_key = key
+        # remove non-word, non-whitespace characters
+        formatted_key = re.sub(r"[^\w\s]", '', formatted_key)
+        # replace whitespace with underscores
+        formatted_key = re.sub(r"\s+", '_', formatted_key)
+        to_return[formatted_key.lower()] = value
+    yield to_return
 
 
 def get_row_iterator(table_spec, reader):
@@ -29,7 +28,7 @@ def get_row_iterator(table_spec, reader):
             json_array = json_array[json_path]
 
         # throw a TypeError if the root json object can not be iterated
-        return generator_wrapper(iter(json_array))
+        return generator_wrapper(json_array)
     except JSONDecodeError as jde:
         if jde.msg.startswith("Extra data"):
             reader.seek(0)
