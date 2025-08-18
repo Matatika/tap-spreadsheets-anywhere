eed37642abc0be1ea6782a171a1ada1af7049d04
danielpdwalker <danielpdwalker@gmail.com>
2025-08-18 14:29:28 +0100

    Added new state_based_discovery setting than for the imap protocol lets a user enable discovering only files since the last_modified state for a stream

--- tap_spreadsheets_anywhere/__init__.py
+++ tap_spreadsheets_anywhere/__init__.py
@@ -62,10 +62,15 @@ def discover(config):
     streams = []
     for table_spec in config['tables']:
         try:
+            state_modified_since = (
+                utils.parse_args(['tables']).state.get(table_spec['name'], {}).get("modified_since")
+                if table_spec.get('state_based_discovery')
+                else None
+            )
             sample_rate = table_spec.get('sample_rate',5)
             max_sampling_read = table_spec.get('max_sampling_read', 1000)
             max_sampled_files = table_spec.get('max_sampled_files', 50)
-            modified_since = dateutil.parser.parse(table_spec['start_date'])
+            modified_since = dateutil.parser.parse(state_modified_since or table_spec['start_date'])
             target_files = file_utils.get_matching_objects(table_spec, modified_since, max_sampled_files)
             sample_rate = table_spec.get('sample_rate',5)
             max_sampling_read = table_spec.get('max_sampling_read', 1000)
