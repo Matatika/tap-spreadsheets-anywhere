8f27d9416322488c9bbbb7a366eaf958f3374347
Reuben Frankel <rfrankel@matatika.com>
2025-08-26 14:01:28 +0100

    Revert "Added a limit to the amount of files obtained by the imap protocol, this limit is the number that we plan to sample as getting any more is just wasting time"
    This reverts commit 323693f767f0204ff1eb4dd155665a9d9b014c78.

--- tap_spreadsheets_anywhere/file_utils.py
+++ tap_spreadsheets_anywhere/file_utils.py
@@ -147,7 +147,7 @@ def parse_path(path):
     return ('local', path_parts[0]) if len(path_parts) <= 1 else (path_parts[0], path_parts[1])
 
 
-def get_matching_objects(table_spec, modified_since=None, max_sampled_files=None):
+def get_matching_objects(table_spec, modified_since=None):
     protocol, bucket = parse_path(table_spec['path'])
 
     # TODO Breakout the transport schemes here similar to the registry/loading pattern used by smart_open
@@ -170,7 +170,6 @@ def get_matching_objects(table_spec, modified_since=None, max_sampled_files=None
             table_spec["path"],
             table_spec.get("search_prefix"),
             modified_since,
-            max_sampled_files,
         )
     else:
         raise ValueError("Protocol {} not yet supported. Pull Requests are welcome!")
@@ -357,13 +356,12 @@ def list_files_in_imap_mailbox(
     uri: str,
     search_prefix: str | None = None,
     modified_since: datetime | None = None,
-    max_sampled_files: int | None = None,
 ):
     parsed = urlparse(uri)
     fs = tap_spreadsheets_anywhere.format_handler.get_imap_fs(parsed.netloc)
     target_objects = []
 
-    for f in fs.ls(parsed.path, since=modified_since.date(), limit=max_sampled_files):
+    for f in fs.ls(parsed.path, since=modified_since.date()):
         if search_prefix is None or fnmatch.fnmatch(f["name"], search_prefix):
             last_modified = f.get("last_modified") or datetime.now(tz=timezone.utc)
             target_objects.append(
